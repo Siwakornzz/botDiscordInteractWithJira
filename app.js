a9181713f377bf5a10b5088ca5af579507401380
Siwakornzz <Siwakornzz@hotmail.com>
2023-02-01 18:05:30 +0700

    update refix

--- app.js
+++ app.js
@@ -6,16 +6,78 @@ const config = process.env;
 const app = express();
 app.use(bodyParser.json());
 
+// @ts-ignore
 app.get("/", async (req, res) => {
   res.status(201).end();
 });
 
+// app.post("/webhook", async (req, res) => {
+//   const issueEventTypeName = await req.body["issue_event_type_name"];
+//   console.log(Object.keys(req.body));
+//   if (issueEventTypeName == "issue_assigned") {
+//     console.log(JSON.stringify(req.body, null, 2));
+//     console.log(issueEventTypeName);
+//   }
+//   res.send("OK");
+// });
+
 app.post("/webhook", async (req, res) => {
-  const issueEventTypeName = await req.body["issue_event_type_name"];
-  console.log(Object.keys(req.body));
-  if (issueEventTypeName == "issue_assigned") {
-    console.log(JSON.stringify(req.body, null, 2));
-    console.log(issueEventTypeName);
+  // console.log(JSON.stringify(req.body,null,2))
+  const issue_event_type_name = await req.body["issue_event_type_name"];
+  if (issue_event_type_name == "issue_assigned") {
+    // @ts-ignore
+    jiraName = await req.body.changelog.items[0].toString;
+    // @ts-ignore
+    workName = await req.body.issue.fields.summary;
+    // @ts-ignore
+    projectName = await req.body.issue.fields.project.name;
+    // @ts-ignore
+    projectDescription = await req.body.issue.fields.description;
+    // console.log("jiraName : ", jiraName);
+    // console.log("workName : ", workName);
+    // console.log("projectName : ", projectName);
+    // console.log("projectDescription :", projectDescription);
+    // @ts-ignore
+    fs.readFile("./data.json", "utf8", async (err, dataUser) => {
+      if (err) {
+        console.log("File read failed:", err);
+        return;
+      }
+      // @ts-ignore
+      dataUserParse = await JSON.parse(dataUser);
+      // @ts-ignore
+      if (dataUserParse[jiraName]) {
+        // @ts-ignore
+        const guild = client.guilds.cache.first();
+        // @ts-ignore
+        guild.members.fetch().then((members) => {
+          guild.members.cache.filter((member) => !member.user.bot);
+          // @ts-ignore
+          const user = client.users.cache.get(dataUserParse[jiraName]);
+          if (user) {
+            user.createDM().then(async (dmChannel) => {
+              // @ts-ignore
+              const exampleEmbed = new EmbedBuilder()
+                .setColor(0x0099ff)
+                // @ts-ignore
+                .setTitle(`${projectName}`)
+                .setDescription("ได้รับมอบหมายงาน")
+                .addFields({
+                  // @ts-ignore
+                  name: `${workName}`,
+                  // @ts-ignore
+                  value: ` \nรายละเอียดงาน : ${projectDescription}`,
+                })
+                .setTimestamp();
+              // dmChannel.send(`${jiraName} ได้รับมอบหมายงาน ${workName} ใน JIRA`);
+              await dmChannel.send({ embeds: [exampleEmbed] });
+            });
+          } else {
+            console.log("not found");
+          }
+        });
+      }
+    });
   }
   res.send("OK");
 });
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const { Client, MessageEmbed } = require("discord.js");
 const client = new Client();
 const config = process.env;
+const fs = require("fs");
+
 require("./app");
 
 client.on("ready", async () => {
@@ -17,37 +19,37 @@ client.on("ready", async () => {
       // console.log("userId : ", iterator[0]);
       user = client.users.cache.get(iterator[0]);
       if (user) {
-        const exampleEmbed = new MessageEmbed()
-          .setColor("#0099ff")
-          .setTitle("Custom Template")
-          .setDescription(
-            "This is a custom message template created using Discord.js"
-          )
-          .setThumbnail("https://i.imgur.com/wSTFkRM.png")
-          .addFields(
-            { name: "Regular field title", value: "Some value here" },
-            { name: "\u200B", value: "\u200B" },
-            {
-              name: "Inline field title",
-              value: "Some value here",
-              inline: true,
-            },
-            {
-              name: "Inline field title",
-              value: "Some value here",
-              inline: true,
-            }
-          )
-          .setImage("https://i.imgur.com/wSTFkRM.png")
-          .setTimestamp()
-          .setFooter(
-            "Custom Template Footer Text",
-            "https://i.imgur.com/wSTFkRM.png"
-          );
-        // console.log(user);
-        user.createDM().then((dmChannel) => {
-          dmChannel.send(exampleEmbed);
-        });
+        // const exampleEmbed = new MessageEmbed()
+        //   .setColor("#0099ff")
+        //   .setTitle("Custom Template")
+        //   .setDescription(
+        //     "This is a custom message template created using Discord.js"
+        //   )
+        //   .setThumbnail("https://i.imgur.com/wSTFkRM.png")
+        //   .addFields(
+        //     { name: "Regular field title", value: "Some value here" },
+        //     { name: "\u200B", value: "\u200B" },
+        //     {
+        //       name: "Inline field title",
+        //       value: "Some value here",
+        //       inline: true,
+        //     },
+        //     {
+        //       name: "Inline field title",
+        //       value: "Some value here",
+        //       inline: true,
+        //     }
+        //   )
+        //   .setImage("https://i.imgur.com/wSTFkRM.png")
+        //   .setTimestamp()
+        //   .setFooter(
+        //     "Custom Template Footer Text",
+        //     "https://i.imgur.com/wSTFkRM.png"
+        //   );
+        // // console.log(user);
+        // user.createDM().then((dmChannel) => {
+        //   dmChannel.send(exampleEmbed);
+        // });
       } else {
         console.log("not found");
       }
@@ -66,21 +68,113 @@ client.on("ready", async () => {
 //   }
 // });
 
+// client.on("message", async (message) => {
+//   if (message) {
+//     if (message.content.includes("!register")) {
+//       // !register username
+//       const username_jira = message.content.replace("!register", "").trim();
+//       const Id_Discord = message.guildId;
+//       await fs.readFile("./data.json", "utf8", async (err, dataUser) => {
+//         if (err) {
+//           console.log("File read failed:", err);
+//           return;
+//         }
+//         console.log("File data:", dataUser);
+//         const dataUserParse = await JSON.parse(dataUser);
+//         dataUserParse[username_jira] = Id_Discord;
+//         await fs.writeFile(
+//           "./data.json",
+//           JSON.stringify(dataUserParse, null, 2),
+//           (err) => {
+//             if (err) {
+//               console.log("Error writing file", err);
+//             } else {
+//               console.log("Successfully wrote file");
+//             }
+//           }
+//         );
+//       });
+//     } else if (message.content.includes("!updateuser")) {
+//       // !updateuser old_user => new_user
+//       const username_jira = message.content
+//         .replace("!updateuser", "")
+//         .trim()
+//         .split("=>");
+//       const old_username_jira = username_jira[0].trim();
+//       const new_username_jira = username_jira[1].trim();
+//       const Id_Discord = message.guildId;
+//       await fs.readFile("./data.json", "utf8", async (err, dataUser) => {
+//         if (err) {
+//           console.log("File read failed:", err);
+//           return;
+//         }
+//         console.log("File data:", dataUser);
+//         const dataUserParse = await JSON.parse(dataUser);
+//         delete dataUserParse[old_username_jira];
+//         dataUserParse[new_username_jira] = Id_Discord;
+//         await fs.writeFile(
+//           "./data.json",
+//           JSON.stringify(dataUserParse, null, 2),
+//           (err) => {
+//             if (err) {
+//               console.log("Error writing file", err);
+//             } else {
+//               console.log("Successfully wrote file");
+//             }
+//           }
+//         );
+//       });
+//     }
+//   }
+// });
+
 client.on("message", async (message) => {
   if (message) {
     if (message.content.includes("!register")) {
       // !register username
-      const username_jira = message.content.replace("!register", "").trim();
-      const Id_Discord = message.guildId;
-      await fs.readFile("./data.json", "utf8", async (err, dataUser) => {
+      username_jira = message.content.replace("!register", "").trim();
+      Id_Discord = message.author.id;
+      fs.readFile("./data.json", "utf8", async (err, dataUser) => {
         if (err) {
           console.log("File read failed:", err);
           return;
         }
         console.log("File data:", dataUser);
-        const dataUserParse = await JSON.parse(dataUser);
+        dataUserParse = await JSON.parse(dataUser);
         dataUserParse[username_jira] = Id_Discord;
-        await fs.writeFile(
+        fs.writeFile(
+          "./data.json",
+          JSON.stringify(dataUserParse, null, 2),
+          (err) => {
+            if (err) {
+              console.log("Error writing file", err);
+              message.react("❌");
+            } else {
+              console.log("Successfully wrote file");
+              message.react("✅");
+            }
+          }
+        );
+      });
+    } else if (message.content.includes("!updateuser")) {
+      // !updateuser old_user => new_user
+      username_jira = message.content
+        .replace("!updateuser", "")
+        .trim()
+        .split("=>");
+      old_username_jira = username_jira[0].trim();
+      new_username_jira = username_jira[1].trim();
+      Id_Discord = message.author.id;
+      fs.readFile("./data.json", "utf8", async (err, dataUser) => {
+        if (err) {
+          console.log("File read failed:", err);
+          return;
+        }
+        dataUserParse = await JSON.parse(dataUser);
+        old_id = dataUserParse[old_username_jira];
+        delete dataUserParse[old_username_jira];
+        dataUserParse[new_username_jira] = old_id;
+        fs.writeFile(
           "./data.json",
           JSON.stringify(dataUserParse, null, 2),
           (err) => {
@@ -92,25 +186,18 @@ client.on("message", async (message) => {
           }
         );
       });
-    } else if (message.content.includes("!updateuser")) {
+    } else if (message.content.includes("!deleteuser")) {
       // !updateuser old_user => new_user
-      const username_jira = message.content
-        .replace("!updateuser", "")
-        .trim()
-        .split("=>");
-      const old_username_jira = username_jira[0].trim();
-      const new_username_jira = username_jira[1].trim();
-      const Id_Discord = message.guildId;
-      await fs.readFile("./data.json", "utf8", async (err, dataUser) => {
+      username_jira = await message.content.replace("!deleteuser", "").trim();
+      Id_Discord = await message.author.id;
+      fs.readFile("./data.json", "utf8", async (err, dataUser) => {
         if (err) {
           console.log("File read failed:", err);
           return;
         }
-        console.log("File data:", dataUser);
-        const dataUserParse = await JSON.parse(dataUser);
-        delete dataUserParse[old_username_jira];
-        dataUserParse[new_username_jira] = Id_Discord;
-        await fs.writeFile(
+        dataUserParse = await JSON.parse(dataUser);
+        delete dataUserParse[username_jira];
+        fs.writeFile(
           "./data.json",
           JSON.stringify(dataUserParse, null, 2),
           (err) => {
